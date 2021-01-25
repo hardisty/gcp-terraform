50b48e88cd7681126f74dae69981e8e4fb6e00b9
hardisty <frank.hardisty@gmail.com>
2021-01-25 14:27:59 -0800

    add actual resources

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("C:/Users/admin/.ssh/gcp-service-creds.json")
+
+  project = "hardisty-terraform-test"
+  region  = "us-west1"
+  zone    = "us-west1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
